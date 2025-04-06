1a026522bd4a8d69746d5c348b6104e3dbb8f26f
Vlada <geazint6@gmail.com>
2025-04-06 17:32:28 +0300

    .

---
+++ my-script.js
@@ -0,0 +1,88 @@
+/*оголошення змінних
+const ageAlice = 20;
+console.log(ageAlice);
+const ageMark = 37;
+console.log(ageMark);*/
+
+
+/*перевизначення значення
+let username = "Mark";
+username = "Alice";
+console.log(username);
+
+let dogName = "Yasia";
+dogName = "Britka";
+console.log(dogName);
+
+let myVariable = " Hello world!";
+myVariable = "JavaScript is fun!";
+console.log(myVariable);*/
+
+
+/*
+let value;
+console.log(value); */
+
+/*
+// оператор typeof
+const quantaty = 20;
+console.log(typeof quantaty);
+const username = "Alice";
+console.log(typeof username);
+const dogName = true;
+console.log(typeof dogName);*/
+
+/*
+//арифметичні дії
+const x = 19;
+const y = 53;
+console.log(x + y);
+
+const a = 43;
+const b = -54;
+console.log(a + b);
+
+const c = 5;
+const d = 3;
+console.log(c % d);
+
+
+let age = 34;
+age += 4;
+console.log(age);
+
+let tomato = 53;
+tomato %= 8;
+console.log(tomato);*/
+
+
+/*
+//рядки
+
+const age = 34;
+const message = "Congratulations! You're " + age + " now!";
+console.log(message);
+
+console.log(1 + "2");
+console.log(1 + "2" + 4);
+console.log(1 + 3 + "5");
+console.log(String(5));
+console.log(String(true));
+console.log(String());*/
+/*
+//guest: Bavovniatko, room: 204;
+const username = "Bavovniatko";
+const room = 204;
+const message = " Welcome " + username + "! Your room is " + room + ".";
+console.log(message);
+
+const a = 5;
+const b = 4;
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+console.log(a == b);
+console.log(5 == 5);
+*/
